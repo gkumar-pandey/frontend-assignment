5dbc0d3a96a04990f1793e07027d9534afc372fe
gkumar-pandey <gautamshekhar078@gmail.com>
2023-12-23 04:26:03 +0530

    display button dropdown functionality done

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { MdOutlineTune, MdDarkMode } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
@@ -8,14 +8,18 @@ import { PrimaryBtnProps } from "./buttonType";
 import { DisplayBtnDropDown } from "../dropdown/Dropdown";
 
 export const DisplayButton = () => {
+  const [showDropDown, setShowDropDown] = useState(false);
   return (
-    <button className="flex items-center border border-gray-300 rounded px-2   shadow ">
+    <button
+      onClick={(e) => setShowDropDown(!showDropDown)}
+      className="flex items-center border border-gray-300 rounded px-2   shadow "
+    >
       <MdOutlineTune />
       <span className="px-2">Display</span>
       <span>
         <IoIosArrowDown />
       </span>
-      {/* <DisplayBtnDropDown /> */}
+      {showDropDown && <DisplayBtnDropDown />}
     </button>
   );
 };
@@ -33,9 +37,9 @@ export const DarkModeButton = () => {
 // Pirmary button
 export const PrimaryButton: FC<PrimaryBtnProps> = ({ children }) => {
   return (
-    <button className="border rounded px-2 flex items-center justify-between w-2/5 ">
+    <div className="border rounded px-2 flex items-center justify-between w-2/5 ">
       {children}
       <IoIosArrowDown />
-    </button>
+    </div>
   );
 };
